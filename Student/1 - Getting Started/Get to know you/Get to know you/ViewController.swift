02ec5f74d5eab343db6f23b49d20a48b925fb785
Adison Green <adison@adisons-mbp.lan>
2021-08-25 17:05:30 -0600

    Light Project

---
+++ Student/1 - Getting Started/Get to know you/Get to know you/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Get to know you
+//
+//  Created by Adison Green on 8/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Student/1 - Getting Started/Guided Project-Light/Guided Project-Light/ViewController.swift
+++ Student/1 - Getting Started/Guided Project-Light/Guided Project-Light/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lightButton: UIButton!
+    var lightOn = true
+        
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
+            updateUI()
+    }
+    
+   func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+   @IBAction func buttonPressed(_ sender: Any) {
+    lightOn.toggle()
+    updateUI()
+    }
+    
+
         // Do any additional setup after loading the view.
     }
 
 
-}
+
 
