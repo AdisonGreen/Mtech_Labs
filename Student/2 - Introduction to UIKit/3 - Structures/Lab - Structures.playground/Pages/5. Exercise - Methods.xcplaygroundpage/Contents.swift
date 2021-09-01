1ddf3405582425da8ab6355e01d2c83d13c3cccf
Adison Green <adison@Adisons-MacBook-Pro.local>
2021-09-01 08:03:11 -0600

    Classes

--- Student/2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ Student/2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -11,16 +11,25 @@ struct Book {
     
 }
 
+let description = Book(title: "Mistborn", author: "Brandon Sanderson", pages: 541, price: 10.0)
+print(description)
 
 //:  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
 struct Post {
     var message: String
     var likes: Int
     var numberOfComments: Int
+    
+    mutating func like() {
+        likes += 1
+    }
 
 }
 
-
+var post = Post(message: "Look at this it is so cool", likes: 0, numberOfComments: 12)
+print(post.likes)
+post.like()
+print(post.likes)
 /*:
 [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Workout Functions](@next)
  */
--- Student/2 - Introduction to UIKit/4 - Classes and Inheritance/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
+++ Student/2 - Introduction to UIKit/4 - Classes and Inheritance/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
@@ -22,7 +22,15 @@ class Spaceship {
             print("Sorry, your ship was hit one too many times. Do you want to play again?")
         }
     }
+    
+    init(name: String, health: Int, position: Int) {
+        self.name = name
+        self.health = health
+        self.position = position
+    }
+
 }
+
 
 class Fighter: Spaceship {
     let weapon: String
@@ -35,7 +43,16 @@ class Fighter: Spaceship {
             print("You have no more fire power.")
         }
     }
+    
+    init(weapon: String, remainingFirePower: Int, name: String, health: Int, position: Int) {
+        self.weapon = weapon
+        self.remainingFirePower = remainingFirePower
+        super.init(name: name, health: health, position: position)
+    }
+       
 }
+    
+
 
 class ShieldedShip: Fighter {
     var shieldStrength: Int
@@ -47,31 +64,40 @@ class ShieldedShip: Fighter {
             super.wasHit()
         }
     }
+    
+    init(shieldStrength: Int, name: String, health: Int, position: Int, weapon: String, remainingFirePower: Int) {
+        self.shieldStrength = shieldStrength
+        super.init(weapon: weapon, remainingFirePower: remainingFirePower, name: name, health: health, position: position)
+    }
 }
 /*:
  Note that each class above has an error by the class declaration that says "Class has no initializers." Unlike structs, classes do not come with memberwise initializers because the standard memberwise initializers don't always play nicely with inheritance. You can get rid of the error by providing default values for everything, but it is common, and better practice, to simply write your own initializer. Go to the declaration of `Spaceship` and add an initializer that takes in an argument for each property on `Spaceship` and sets the properties accordingly.
 
  Then create an instance of `Spaceship` below called `falcon`. Use the memberwise initializer you just created. The ship's name should be "Falcon."
  */
-
+var falcon = Spaceship(name: "Falcon", health: 100, position: 0)
 
 /*:
  Writing initializers for subclasses can get tricky. Your initializer needs to not only set the properties declared on the subclass, but also set all of the uninitialized properties on classes that it inherits from. Go to the declaration of `Fighter` and write an initializer that takes an argument for each property on `Fighter` and for each property on `Spaceship`. Set the properties accordingly. (Hint: you can call through to a superclass' initializer with `super.init` *after* you initialize all of the properties on the subclass).
 
  Then create an instance of `Fighter` below called `destroyer`. Use the memberwise initializer you just created. The ship's name should be "Destroyer."
  */
-
+var destroyer = Fighter(weapon: "Laser", remainingFirePower: 10, name: "Destroyer", health: 100, position: 0)
 
 /*:
  Now go add an initializer to `ShieldedShip` that takes an argument for each property on `ShieldedShip`, `Fighter`, and `Spaceship`, and sets the properties accordingly. Remember that you can call through to the initializer on `Fighter` using `super.init`.
 
  Then create an instance of `ShieldedShip` below called `defender`. Use the memberwise initializer you just created. The ship's name should be "Defender."
  */
-
-
+var denfender = ShieldedShip(shieldStrength: 100, name: "Defender", health: 200, position: 0, weapon: "Laser", remainingFirePower: 10)
 //:  Create a new constant named `sameShip` and set it equal to `falcon`. Print out the position of `sameShip` and `falcon`, then call `moveLeft()` on `sameShip` and print out the position of `sameShip` and `falcon` again. Did both positions change? Why? If both were structs instead of classes, would it be the same? Why or why not? Provide your answer in a comment or print statement below.
-
-
+let sameShip = falcon
+print(falcon.position)
+print(sameShip.position)
+sameShip.moveLeft()
+print(falcon.position)
+print(sameShip.position)
+print("They were the same and that is because class have references. They are stored in the same memory in the computer but if you were to use a struct it would be differnt, they store in differnt memories on the computer.")
 /*:
  _Copyright © 2021 Apple Inc._
 
