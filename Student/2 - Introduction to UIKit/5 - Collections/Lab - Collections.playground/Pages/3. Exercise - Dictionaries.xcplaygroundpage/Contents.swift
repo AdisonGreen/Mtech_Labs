6cff865552442f9d509939f2f4a2d1a4ab9c2dfb
Adison Green <adison@Adisons-MacBook-Pro.local>
2021-09-08 07:45:34 -0600

    Loops

--- Student/2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ Student/2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -3,16 +3,16 @@
 
  Create a variable `[String: Int]` dictionary that can be used to look up the number of days in a particular month. Use a dictionary literal to initialize it with January, February, and March. January contains 31 days, February has 28, and March has 31. Print the dictionary.
  */
-var Months = ["Januray": 31, "February": 28, "March": 31]
-print(Months)
+var months = ["Januray": 31, "February": 28, "March": 31]
+print(months)
 //:  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
-Months["April"] = 30
-print(Months)
+months["April"] = 30
+print(months)
 //:  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
-Months.updateValue(29, forKey: "February")
-print(Months)
+months.updateValue(29, forKey: "February")
+print(months)
 //:  Use if-let syntax to retrieve the number of days under "January". If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
-if let januaryDays = Months["Januray"] {
+if let januaryDays = months["Januray"] {
     print("January has \(januaryDays) days")
 }
 
--- Student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
+++ Student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -7,11 +7,15 @@
 
 //:  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
-
+for (index, letters) in alphabet.enumerated() {
+    print("\(index): \(letters)")
+}
 
 //:  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
-
-
+var states: [String: String] = ["Utah": "Salt Lake City", "Colorado": "Denver", "Nevada": "Carson City"]
+for capitals in states {
+    print("\(capitals.value) is the capital of \(capitals.key)")
+}
 /*:
 page 1 of 6  |  [Next: App Exercise - Movements](@next)
  */
